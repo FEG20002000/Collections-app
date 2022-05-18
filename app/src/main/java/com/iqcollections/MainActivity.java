0a0f36d9b115c5f7f0f7154275d9e366d0edba7d
msilv <highiqcollections@gmail.com>
2022-05-18 14:39:37 +0200

    g

--- app/src/main/java/com/iqcollections/MainActivity.java
+++ app/src/main/java/com/iqcollections/MainActivity.java
@@ -1,14 +1,46 @@
 package com.iqcollections;
 
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.iqcollections.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        //adding name of collection pull from Database
+        String[] collectionName = {"Car","IT","PC"};
+       //adding images to an array must pull from database
+        int[] collectionIMg = {R.drawable.background,R.drawable.background,R.drawable.background};
+
+        grid_adapter gridAdapter = new grid_adapter(MainActivity.this, collectionName, collectionIMg) {
+        };
+        binding.grdMain.setAdapter(gridAdapter);
+
+
+        binding.grdMain.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Toast.makeText(MainActivity.this,"You Clicked on "+ collectionName[position],Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_drawer,menu);
+        return true;
     }
 }
