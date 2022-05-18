7736ca8fa09941de8bbb9ea2b006b0c9a5a66308
msilv <highiqcollections@gmail.com>
2022-05-18 14:51:24 +0200

    g

---
+++ app/src/main/java/com/iqcollections/createCollection.java
@@ -0,0 +1,14 @@
+package com.iqcollections;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class createCollection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_collection);
+    }
+}
---
+++ app/src/main/java/com/iqcollections/createItem.java
@@ -0,0 +1,14 @@
+package com.iqcollections;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class createItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_item);
+    }
+}
