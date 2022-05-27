a4d8dfbce65c56d79a249f747c533d7a06fa5f04
msilv <highiqcollections@gmail.com>
2022-05-27 08:36:28 +0200

    g

--- app/src/main/java/com/iqcollections/createItem.java
+++ app/src/main/java/com/iqcollections/createItem.java
@@ -3,8 +3,18 @@ package com.iqcollections;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.type.Date;
+import com.google.type.DateTime;
 
 public class createItem extends AppCompatActivity {
+
+    private EditText edtItemName,edtItemDescription;
+    private View dtitemDate;
+    private Button btnImageSelect,btnCreate;
 
 
     @Override
@@ -13,7 +23,16 @@ public class createItem extends AppCompatActivity {
         setContentView(R.layout.activity_create_item);
 
 
+        edtItemDescription = findViewById(R.id.edtitemDesc);
+        edtItemName = findViewById(R.id.edtItemname);
+        dtitemDate = findViewById(R.id.dtpItemDate);
+        btnImageSelect = findViewById(R.id.selectitemimg);
+        btnCreate = findViewById(R.id.btnCreateitem);
+
+
 
 
     }
+
+
 }
