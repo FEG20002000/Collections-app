d97bbc63edc6840b4b35dd9b11d3735770af20ea
msilv <highiqcollections@gmail.com>
2022-05-29 11:35:13 +0200

    g

--- app/src/main/java/com/iqcollections/itemDetail.java
+++ app/src/main/java/com/iqcollections/itemDetail.java
@@ -3,12 +3,33 @@ package com.iqcollections;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class itemDetail extends AppCompatActivity {
+
+    private TextView name,description,date;
+    private ImageView imgItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_detail);
+
+        name = findViewById(R.id.txtitemname);
+        description = findViewById(R.id.txtitemDescrip);
+        date = findViewById(R.id.txtitemDate);
+        imgItem = findViewById(R.id.imgitemView);
+        try{
+
+
+
+        }catch (Exception e){
+
+        }
+
+
+
+
     }
 }
