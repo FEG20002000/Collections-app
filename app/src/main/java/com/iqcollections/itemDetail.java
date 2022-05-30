179587bcc7daca4502a583fe4cb264963657efea
msilv <highiqcollections@gmail.com>
2022-05-30 10:30:14 +0200

    list item

--- app/src/main/java/com/iqcollections/itemDetail.java
+++ app/src/main/java/com/iqcollections/itemDetail.java
@@ -2,15 +2,22 @@ package com.iqcollections;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class itemDetail extends AppCompatActivity {
 
     private TextView name,description,date;
     private ImageView imgItem;
     private String selectedItem;
+    private FirebaseUser uid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +30,8 @@ public class itemDetail extends AppCompatActivity {
         imgItem = findViewById(R.id.imgitemView);
         listItem lstIt = new listItem();
         selectedItem = lstIt.getSelectedItem();
-
+        uid = FirebaseAuth.getInstance().getCurrentUser();
+        Query checkitem = FirebaseDatabase.getInstance().getReference("Items").child(uid.getUid()).orderByChild("itemName").equalTo(selectedItem);
         try{
 
 
