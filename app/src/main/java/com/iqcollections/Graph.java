4e845643213c0b14e233e2ff1e33e75efe09a70c
msilv <highiqcollections@gmail.com>
2022-06-22 23:25:30 +0200

    piechart

--- app/src/main/java/com/iqcollections/Graph.java
+++ app/src/main/java/com/iqcollections/Graph.java
@@ -9,6 +9,7 @@ import android.view.MenuItem;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
@@ -26,8 +27,12 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -42,13 +47,20 @@ public class Graph extends AppCompatActivity implements NavigationView.OnNavigat
 
     private ListView wishlistView;
     private FirebaseUser uid;
+    private DatabaseReference db,dbref;
+
 
     ArrayList<listItem> listItemsAL = new ArrayList<>();
-
+    ArrayList<readColGraph> readColGraphs = new ArrayList<>();
+    ArrayList<Float> colNumbers = new ArrayList<Float>();
+    ArrayList<BarEntry> barEntryArrayList = new ArrayList<>();
+    ArrayList<PieEntry> pieEntryArrayList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
+        barGraph = findViewById(R.id.barGraph);
+        pieChart = findViewById(R.id.pieChart);
 
         // hooks
         dl = findViewById(R.id.graphLayout);
@@ -65,25 +77,128 @@ public class Graph extends AppCompatActivity implements NavigationView.OnNavigat
 
         // Graph starts from here
 
-        barGraph = findViewById(R.id.barGraph);
-        pieChart = findViewById(R.id.pieChart);
 
         //Initialize array list
-        ArrayList<BarEntry> barEntryArrayList = new ArrayList<>();
-        ArrayList<PieEntry> pieEntryArrayList = new ArrayList<>();
+
         //ArrayList<String> barLblNames;
         //ArrayList<String> list = new ArrayList<>();//Using an arraylist to store the data from firebase
+        //getting items from database
 
+        db = FirebaseDatabase.getInstance().getReference("Collections").child(uid.getUid());
+        dbref = FirebaseDatabase.getInstance().getReference("Items").child(uid.getUid());
+
+
+        db.addChildEventListener(new ChildEventListener() {//searching though data
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                readColGraph collections = snapshot.getValue(readColGraph.class);
+                readColGraphs.add(collections);
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        db.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                countitems();//once data is stored couning items
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+    public void countitems() {
+        for (int i = 0; i < readColGraphs.size();i++){//counting items for each collection
+            int e = i;
+            final int[] counter = {0};
+        dbref.addChildEventListener(new ChildEventListener() {//checking each item
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                readItemGraph value = snapshot.getValue(readItemGraph.class);
+
+                    if (readColGraphs.get(e).getColName().equals(value.getItemCollection())) {
+                        counter[0]++;
+                    }
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+            int f = i;
+            dbref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                colNumbers.add(f, (float) counter[0]);//adding number of items
+                if(colNumbers.size()>= readColGraphs.size()){
+                    setPieChart();//setting piechart only if all counts are done
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    }
+
+    public void setPieChart(){
+        pieChart.clear();
         //Use for loop for the graph data and to get the data
-        for (int i = 0; i < listItemsAL.size();i++){
-            String itemId = listItemsAL.get(i).getSelectedItem();
+        for (int i = 0; i < readColGraphs.size();i++){
+            String itemId = readColGraphs.get(i).getColName();//data
+            Float itemAmounts= colNumbers.get(i);//data
             //Initialize bar chart entry
-            BarEntry barEntry = new BarEntry(i, Float.parseFloat(itemId));
+          //BarEntry  barEntry = new BarEntry(itemAmounts, Float.parseFloat(itemId));
             //Initialize the pie chart entry
-            PieEntry pieEntry = new PieEntry(i, Float.parseFloat(itemId));
+            PieEntry pieEntry = new PieEntry(itemAmounts,itemId);
             //add the values to the arraylist
-            barEntryArrayList.add(barEntry);
-            pieEntryArrayList.add(pieEntry);
+           // barEntryArrayList.add(barEntry);
+            pieEntryArrayList.add(new PieEntry(itemAmounts,itemId));
 
         }
         // Initialize the bar data set
@@ -105,17 +220,18 @@ public class Graph extends AppCompatActivity implements NavigationView.OnNavigat
         // set the colors for the bar graph
         pieDataSetItems.setColors(ColorTemplate.COLORFUL_COLORS);
         //Hide draw value
-        pieDataSetItems.setDrawValues(false);
+       //pieDataSetItems.setDrawValues(true);
         //Set the bar data
         pieChart.setData((new PieData(pieDataSetItems)));
         //set the animation of the graph
-        pieChart.animateXY(5000 , 5000);
+        pieChart.animateY(5000 );
         //set the description of the graph text and color
         pieChart.getDescription().setText("Items Chart");
         pieChart.getDescription().setTextColor(Color.BLUE);
 
 
     }
+
 
     //do not delete this is for the options menu buttons
     public boolean onCreateOptionsMenu(Menu menu) {
