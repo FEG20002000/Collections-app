43b3527519d6bef57f7d7d678791812f30f10d0e
msilv <highiqcollections@gmail.com>
2022-06-25 13:41:29 +0200

    Take photo

--- app/src/main/java/com/iqcollections/createCollection.java
+++ app/src/main/java/com/iqcollections/createCollection.java
@@ -16,11 +16,15 @@ package com.iqcollections;
     Author name/tag/channel: Cambo Tutorial
     Author channel/profile url link: https://m.youtube.com/c/CamboTutorial
 */
+import android.Manifest;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.MenuItem;
 import android.view.View;
 import android.webkit.MimeTypeMap;
@@ -29,10 +33,17 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -47,8 +58,14 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+
 public class createCollection extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    private Button createCollection, imgSelection;
+    private Button createCollection, imgSelection,btnCamera;
     private FirebaseAuth mAuth;
     private EditText name, description, goal;
     private ImageView imgDisplay;
@@ -60,7 +77,7 @@ public class createCollection extends AppCompatActivity implements NavigationVie
     Boolean imgSelected = false;
     DrawerLayout dl;
     NavigationView nv;
-
+    ActivityResultLauncher<Intent> activityResultLauncher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -74,6 +91,7 @@ public class createCollection extends AppCompatActivity implements NavigationVie
             imgSelection = findViewById(R.id.selectImg);
             createCollection = findViewById(R.id.btnCreateColl);
             goal = findViewById(R.id.edtGoal);
+            btnCamera = findViewById(R.id.selectCamera);
 
             dl = findViewById(R.id.createColLayout);
             nv = findViewById(R.id.nav_view);
@@ -98,6 +116,34 @@ public class createCollection extends AppCompatActivity implements NavigationVie
                 }
             });
 
+            btnCamera.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                        if(ContextCompat.checkSelfPermission(createCollection.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                            ActivityCompat.requestPermissions(createCollection.this,new String[]{Manifest.permission.CAMERA},100);
+                        }
+                        if(ContextCompat.checkSelfPermission(createCollection.this,Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+                            ActivityCompat.requestPermissions(createCollection.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},100);
+                        }
+
+                        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        activityResultLauncher.launch(cameraIntent);
+                        imgSelected = true;
+                    }
+                });
+
+                activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        Bundle extras =result.getData().getExtras();
+                        Bitmap bitmap = (Bitmap) extras.get("data");
+                        WeakReference<Bitmap> result1 =  new WeakReference<>(Bitmap.createScaledBitmap(bitmap,bitmap.getHeight(),bitmap.getWidth(),false).copy(
+                                Bitmap.Config.RGB_565,true));
+                        Bitmap bm = result1.get();
+                        imgURI = saveimage(bm,createCollection.this);
+                        imgDisplay.setImageURI(imgURI);
+                    }
+                });
             createCollection.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -185,6 +231,25 @@ public class createCollection extends AppCompatActivity implements NavigationVie
                 imgDisplay.setImageURI(imgURI);//setting image data
             }
         }
+    }
+    private Uri saveimage(Bitmap image,Context context ){
+        File imageFolder = new File(context.getCacheDir(),"images");
+        Uri uri = null;
+        try{
+            imageFolder.mkdirs();
+            File file = new File(imageFolder,"captured_image.jpg");
+            FileOutputStream stream = new FileOutputStream(file);
+            image.compress(Bitmap.CompressFormat.JPEG,100,stream);
+            stream.flush();
+            stream.close();
+            uri = FileProvider.getUriForFile(context.getApplicationContext(),BuildConfig.APPLICATION_ID+".provider", file);
+
+        }catch (FileNotFoundException e){
+            Toast.makeText(context, "There was a file error", Toast.LENGTH_SHORT).show();
+        }catch (IOException er){
+            Toast.makeText(context, "There was an Store error", Toast.LENGTH_SHORT).show();
+        }
+        return uri;
     }
 
     @Override//nav bar
